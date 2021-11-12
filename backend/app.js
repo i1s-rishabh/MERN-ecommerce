f6080eb41a0f6cd09856a23378979715d60f33d1
ANKUR <ankur02srma@gmail.com>
2021-11-12 23:33:40 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const errorMiddleware = require('./middleware/error')
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
 
-app.use(express.json())
+const errorMiddleware = require("./middleware/error");
 
-// route imports
-const product = require("./routes/productRoute")
-const user = require("./routes/userRoute")
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 
-app.use("/api/v1", product)
-app.use("/api/v1/", user)
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 
-// Middleware for errors
+// Route Imports
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
+
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
+// Middleware for Errors
 app.use(errorMiddleware);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,38 @@
-const app = require('./app');
+const app = require("./app");
+const cloudinary = require("cloudinary");
+const connectDatabase = require("./config/database");
 
-const dotenv = require('dotenv');
-const connectDatabase = require('./config/database');
-
-// Handling uncaugh exception
-process.on('uncaughtException', err => {
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
-  console.log('Shutting down the server due to uncaugh exception');
+  console.log(`Shutting down the server due to Uncaught Exception`);
   process.exit(1);
-})
+});
 
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 
-// configuration
-dotenv.config({path:"backend/config/config.env"})
+// Connecting to database
+connectDatabase();
 
-// connecting to database
-connectDatabase()
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is working on http://localhost:${process.env.PORT}`);
-  });
-  
-// Unhandled promise rejection 
-process.on('unhandledRejection', err => {
-  console.log(`Err: ${err.message}`);
-  console.log('Shutting down the server due to unhandled promise rejection')
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
   server.close(() => {
     process.exit(1);
-  })
-})
+  });
+});
